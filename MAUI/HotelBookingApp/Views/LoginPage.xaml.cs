a3eac35ada94104bef3d63f553d6aef7abc969f0
ROSE329-hub <gennaroseguardacasa@gmail.com>
2026-07-14 03:02:46 +0800

    add LoginPage UI

---
+++ MAUI/HotelBookingApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace HotelBookingApp
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text?.Trim() ?? "";
+            string password = txtPassword.Text ?? "";
+
+            if (email == "user@gmail.com" && password == "123456") //hardcoded credentials for demonstration purposes    
+            {
+                await DisplayAlert("Success", "Login Successful!", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error", "Invalid email or password.", "OK");
+            }
+        }
+
+    }
+}
