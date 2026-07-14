17c5a3b4ba3e3bbefa88d7edc99b5e3b6669ccf2
honeyglazed02 <hnnhgrc2005@gmail.com>
2026-07-14 17:16:52 +0800

    add payment page

---
+++ MAUI/HotelBookingApp/Views/PaymentPage.xaml.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace HotelBookingApp.Views;
+
+public partial class PaymentPage : ContentPage
+{
+    public PaymentPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void Pay_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Payment", "Payment feature is under development.", "OK"); //you can replace this with actual payment processing logic if you want
+    }
+
+    private async void Cancel_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync(); //bring back to booking page
+    }
+}
