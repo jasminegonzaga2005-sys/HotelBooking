cdf683f36bebd425acad21e649a17bbf2bacbb0d
honeyglazed02 <hnnhgrc2005@gmail.com>
2026-07-14 15:16:24 +0800

    Add dashboardPage.xaml and .xaml.cs

---
+++ MAUI/HotelBookingApp/Views/DashboardPage.xaml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingApp.Views;
+
+public partial class DashboardPage : ContentPage
+{
+    public DashboardPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void BookRoom_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Book Room", "This page is under development.", "OK");
+
+        // Later:
+        // await Navigation.PushAsync(new BookARoomPage());
+    }
+
+    private async void MyBookings_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("My Bookings", "This page is under development.", "OK");
+    }
+
+    private async void Home_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Home", "You are already on the Homepage.", "OK");
+    }
+
+    private async void Profile_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Profile", "This page is under development.", "OK");
+    }
+
+    private async void StandardRoom_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Standard Room", "Room details will be shown here.", "OK");
+    }
+
+    private async void SuperiorRoom_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Superior Room", "Room details will be shown here.", "OK");
+    }
+
+    private async void DeluxeRoom_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Deluxe Room", "Room details will be shown here.", "OK");
+    }
+
+    private async void FamilyRoom_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Family Room", "Room details will be shown here.", "OK");
+    }
+
+    private async void ExecutiveSuite_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Executive Suite", "Room details will be shown here.", "OK");
+    }
+}
